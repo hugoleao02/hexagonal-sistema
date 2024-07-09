5bbed7c005c69bee78a071fbadc189d6b622b47e
Hugo <h.ferreira147@gmail.com>
2024-07-09 20:55:08 -0300

    feat: Alterando module.info

--- src/module-info.java
+++ src/module-info.java
@@ -4,19 +4,22 @@ module conta.sistema {
 
     requires spring.tx;
 
-
-
     // expondo porta de entrada (driver)
-
     exports conta.sistema.casouso.imp;
+    exports conta.sistema.casouso.porta;
 
     // expondo sistema negocio
-
     exports conta.sistema.dominio.servico;
+    exports conta.sistema.dominio.modelo;
 
     // expondo adaptadores de saídas (driven)
-
     exports conta.adaptador;
+    exports conta.sistema.porta;
+
+    opens conta.sistema.casouso.porta;
+    opens conta.sistema.casouso.imp;
+    opens conta.sistema.casouso.servico;
+    opens conta.adaptador;
 
     exports teste.unidade.dominio.modelo;
 
